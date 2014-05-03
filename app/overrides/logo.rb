9b6294b52592a331ecdea185ec28a0df1adf5731
Venkatesh Rao <Venkatesh3007@gmail.com>
2014-05-03 10:00:42 +0000

    Hold this

---
+++ app/overrides/logo.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(virtual_path: "spree/shared/header",
+                     name: "logo",
+                     replace_contents: "figure#logo",
+                     text: "Store")
