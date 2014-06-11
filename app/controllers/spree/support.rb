ed70aea805a0cc4966af06d4c10c07d1fff16403
Venkatesh Rao <Venkatesh3007@gmail.com>
2014-06-11 06:05:56 +0000

    Update site

--- app/controllers/spree/support.rb
+++ app/controllers/spree/support.rb
@@ -1,12 +1,16 @@
 module CurrentTenant
   
   def current_tenant
-    Tenant.find_by_subdomain! request.subdomain
+    if request.subdomain == "venky" || request.subdomain == "dhoni"
+      Tenant.find_by_subdomain! request.subdomain
+    else
+      redirect_to 'http://venky.roroid.asia'
+    end
   end
 
   private
     def scope_current_tenant
-      Tenant.current_id = current_tenant.id
+      Tenant.current_id = current_tenant.id if current_tenant
       yield
     ensure
       Tenant.current_id = nil
