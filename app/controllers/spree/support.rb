a262c95119dced4c676b48898f563a0e70b0fbd0
Venkatesh Rao <Venkatesh3007@gmail.com>
2014-07-30 10:56:48 +0000

    Add subdomain limitless-sea-8975

--- app/controllers/spree/support.rb
+++ app/controllers/spree/support.rb
@@ -2,7 +2,7 @@ module CurrentTenant
   
   def current_tenant
     zoho = "https://www.zoho.com/mail/login.html"
-    if request.subdomain.eql?("venky") || request.subdomain.eql?("dhoni") || request.subdomain.eql?("tarun") || request.subdomain.eql?("shivank")
+    if request.subdomain.eql?("limitless-sea-8975") || request.subdomain.eql?("venky") || request.subdomain.eql?("dhoni") || request.subdomain.eql?("tarun") || request.subdomain.eql?("shivank")
       Tenant.find_by_subdomain! request.subdomain
     elsif request.subdomain.eql?("mail")
       redirect_to zoho
